91b019abf3e58bfacfdfd09addd71ba15eb4c6b1
sandra-lewis <sandra.lewis279@gmail.com>
2020-09-18 14:54:34 +0530

    Photo Added

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -1,28 +1,39 @@
 import React from 'react';
+import photo from '../../images/personal-photo.png';
 
 const About = () => {
   return (
-    <div className="container">
-      <div
-        data-aos="fade-right"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-once="true"
-      >
-        <h1 className="center">HI, I'M</h1>
-        <br />
-        <h2>SANDRA REBECCA LEWIS</h2>
+    <div className="container grid-lg">
+      <div>
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-once="true"
+        >
+          <h1 className="align">HI, I'M</h1>
+          <br />
+          <h2>SANDRA REBECCA LEWIS</h2>
+        </div>
+        <div
+          data-aos="fade"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-once="true"
+        >
+          <p className="about-me">
+            A web development enthusiast, currently pursuing my Master's in
+            Computer Applications at Manipal Institute of Technology, Manipal.
+          </p>
+        </div>
       </div>
       <div
         data-aos="fade"
-        data-aos-duration="1000"
+        data-aos-duration="1500"
         data-aos-easing="ease-in-out"
         data-aos-once="true"
       >
-        <p className="about-me">
-          A web development enthusiast, currently pursuing my Master's in
-          Computer Applications at Manipal Institute of Technology, Manipal.
-        </p>
+        <img src={photo} alt="" className="personal-photo" />
       </div>
     </div>
   );
